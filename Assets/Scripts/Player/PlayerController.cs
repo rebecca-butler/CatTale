cb1392707ffa39da31f0ee8553bfde6679e55331
rebecca-butler <rebecca.erin.butler@gmail.com>
2022-11-23 16:01:32 -0800

    Implement collision detection

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,30 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     public float moveSpeed;
+    public LayerMask solidObjectsLayer;
 
     private bool isMoving;
     private Vector2 input;
 
     private Animator animator;
 
-    private void Awake()
-    {
+    private void Awake() {
         animator = GetComponent<Animator>();
     }
 
     /* Update player position */
-    private void Update()
-    {
+    private void Update() {
         if (!isMoving) {
+            // Get user input from arrow keys
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
 
             // Don't allow diagonal movement
             if (input.x != 0) input.y = 0;
 
+            // If input is non-zero, move player
             if (input != Vector2.zero) {
                 animator.SetFloat("moveX", input.x);
                 animator.SetFloat("moveY", input.y);
@@ -34,7 +34,10 @@ public class PlayerController : MonoBehaviour
                 targetPos.x += input.x;
                 targetPos.y += input.y;
 
-                StartCoroutine(Move(targetPos));
+                // Only move player if target position is walkable
+                if (IsWalkable(targetPos)) {
+                    StartCoroutine(Move(targetPos));
+                }
             }
         }
 
@@ -42,8 +45,7 @@ public class PlayerController : MonoBehaviour
     }
 
     /* Coroutine to move player towards target */
-    IEnumerator Move(Vector3 targetPos)
-    {
+    IEnumerator Move(Vector3 targetPos) {
         isMoving = true;
 
         while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon) {
@@ -53,5 +55,15 @@ public class PlayerController : MonoBehaviour
 
         transform.position = targetPos;
         isMoving = false;
+    }
+
+    /* Check if target position is a walkable tile */
+    private bool IsWalkable(Vector3 targetPos) {
+        // Check if a circle centered at the target position with radius of 0.3 
+        // overlaps with a solid object - if so, tile is not walkable 
+        if (Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer) != null) {
+            return false;
+        }
+        return true;
     }
 }
